087d4baa6a173c34cd1b5fe547f3fca255872b38
MaximeLeduey <91879887+MaximeLeduey@users.noreply.github.com>
2021-12-13 11:46:26 +0100

    système surveillance de base

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
-const mouseActivity = () => {
-    alert("bouge ton cul !");
+let keyboard;
+let count = 0;
+
+
+document.addEventListener('keydown', function() {
+    keyboard = false;
+    count++;
+})
+
+const control = () => {
+    if ( count > 0 ) {
+        console.log("c'est bien continue")
+    }
+    else {
+        console.log("travaille");
+    }
+    count = 0;
 }
 
-mouseActivity();
+
+setInterval(control, 36000000);
 
